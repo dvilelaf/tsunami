37656d66c3be73f420abb07e2cf0636001921b5a
dvilela <dvilelaf@gmail.com>
2024-03-18 15:47:11 +0100

    fix: run service script

--- run_service.sh
+++ run_service.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Set env vars
-export $(grep -v '^#' .env | xargs)
+REPO_PATH=$PWD
 
 # Push packages and fetch service
 make clean
@@ -14,8 +13,12 @@ autonomy fetch --local --service dvilela/tsunami && cd tsunami
 autonomy init --reset --author dvilela --remote --ipfs --ipfs-node "/dns/registry.autonolas.tech/tcp/443/https"
 autonomy build-image
 
+# Copy .env file
+cp $REPO_PATH/.env .
+
 # Copy the keys and build the deployment
-cp $KEY_DIR/keys.json .
+cp $REPO_PATH/keys.json .
+
 autonomy deploy build -ltm
 
 # Run the deployment
